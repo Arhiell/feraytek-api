47bf135dd2c379c6b180d8e9ad3fb6643725f126
Arhiell <arielayala907.hz@gmail.com>
2025-10-11 10:56:52 -0300

    Estructura inicial del modelo Producto (tabla productos, relaciones y comentarios explicativos)

---
+++ src/models/producto.model.js
@@ -0,0 +1,12 @@
+/**
+ * ===============================================
+ * Modelo: Producto
+ * Descripción: Representa la tabla "productos" de la BD.
+ * ===============================================
+ */
+
+const pool = require("../config/database");
+
+// TODO: se implementarán las funciones CRUD en la siguiente etapa
+// sOLO ESTAMOS CREANDO ARCHIVOS DE PRUEBAS PARA LUEGO COMPLETARLOS
+module.exports = {};

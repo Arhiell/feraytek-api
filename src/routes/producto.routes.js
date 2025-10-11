b8c63e48ca0b9436f59a360df54dd1439ecb7f0c
Arhiell <arielayala907.hz@gmail.com>
2025-10-11 10:58:56 -0300

    Definición de rutas base para productos (estructura inicial y comentarios)

---
+++ src/routes/producto.routes.js
@@ -0,0 +1,17 @@
+/**
+ * ===============================================
+ * Rutas: Producto
+ * Descripción: Define los endpoints REST de productos.
+ * ===============================================
+ */
+
+const express = require("express");
+const router = express.Router();
+
+// TODO: enlazar controladores
+// Ejemplo futuro:
+// router.get("/", productoController.obtenerTodos);
+
+module.exports = router;
+// POR AHORA SOLO CREAMOS EL ARCHIVO PARA LUEGO COMPLETARLO
+// LEO FIJATE QUE SOLO CREAMOS EL ARCHIVO PARA LUEGO COMPLETARLO

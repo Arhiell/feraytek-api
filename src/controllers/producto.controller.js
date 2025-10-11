7bed61f130bd995243916854b9f59abf66051565
Arhiell <arielayala907.hz@gmail.com>
2025-10-11 10:58:17 -0300

    Creación del controlador ProductoController (estructura base y comentarios explicativos)

---
+++ src/controllers/producto.controller.js
@@ -0,0 +1,13 @@
+/**
+ * ===============================================
+ * Controlador: ProductoController
+ * Descripción: Recibe las peticiones HTTP y coordina
+ * la lógica de negocio con el servicio de productos.
+ * ===============================================
+ */
+
+const productoService = require("../services/producto.service");
+
+// TODO: agregar métodos CRUD
+module.exports = {};
+// POR AHORA SOLO CREAMOS EL ARCHIVO PARA LUEGO COMPLETARLO

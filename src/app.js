cab23ddf8d3f6b6863e8e2b1bb538ea5941c3417
Arhiell <arielayala907.hz@gmail.com>
2025-10-11 17:22:09 -0300

    Integración del módulo Carrito en Express con prefijo /api/carrito

--- src/app.js
+++ src/app.js
@@ -10,23 +10,23 @@ const productoRoutes = require("./routes/producto.routes");
 // Crear instancia de Express para la aplicación principal
 const app = express();
 
-// ----------------------------------------------------------------------
+// Rutas y controladores para el carrito de compras
+const carritoRoutes = require("./routes/carrito.routes");
+
 // Middlewares globales en que se aplican a todas las rutas
-// ----------------------------------------------------------------------
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-// ----------------------------------------------------------------------
 // Rutas principales que nos traen los endpoints de cada módulo
-// ----------------------------------------------------------------------
 app.use("/api/productos", productoRoutes);
 
-// ----------------------------------------------------------------------
 // Ruta de prueba raíz asi sabemos que el servidor está activo
-// ----------------------------------------------------------------------
 app.get("/", (req, res) => {
   res.json({ message: "API Feraytek - Servidor activo, Don Señor ARIELO" });
 });
+
+// Rutas y controladores para el carrito de compras
+app.use("/api/carrito", carritoRoutes);
 
 module.exports = app;
